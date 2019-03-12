1f993bf98032109305dc97d3145cbb6ae40ea4a2
Michael <michael.github@eipsoftware.com>
2019-03-11 21:18:32 -0400

    2019-03-11 assignment one

---
+++ ISM6225_AssignmentOne/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ISM6225_AssignmentOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
